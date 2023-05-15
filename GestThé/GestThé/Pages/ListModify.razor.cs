c06fab44ea27891d091bc70303cb86f11515e39c
Sébastien Duruz <sebastienduruz@protonmail.com>
2023-05-15 16:24:41 +0200

    Add the different components (empty for now) + base structure of the page

---
+++ GestThé/GestThé/Pages/ListModify.razor.cs
@@ -0,0 +1,16 @@
+﻿/**
+* ETML
+* Author : Sébastien Duruz
+* Date : 15.05.2023
+* Description : ListModify Component logic
+*/
+
+namespace GestThé.Pages;
+
+/// <summary>
+/// Class ListModify
+/// </summary>
+public partial class ListModify
+{
+    
+}
