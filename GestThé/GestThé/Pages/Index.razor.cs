ab5f4390948bc3c3bc2b003dab5bf400008343db
Sébastien Duruz <sebastienduruz@protonmail.com>
2023-05-17 13:23:50 +0200

    Add pages layout for the teas page (homepage) and tea detail page

--- GestThé/GestThé/Pages/Index.razor.cs
+++ GestThé/GestThé/Pages/Index.razor.cs
@@ -5,19 +5,68 @@
 * Description : Index Component logic
 */
 
+using System.Collections;
+using ElectronNET.API.Entities;
 using GestThéLib.Models.Database;
 using Microsoft.EntityFrameworkCore;
+using Radzen.Blazor;
 
 namespace GestThé.Pages;
 
 public partial class Index
 {
     /// <summary>
+    /// Show Archived tea on the datagrid ?
+    /// </summary>
+    private bool _showArchived;
+    
+    /// <summary>
+    /// List of the teas
+    /// </summary>
+    private IQueryable<TTea> CollectionTeas { get; set; }
+
+    /// <summary>
+    /// Datagrid for the teas
+    /// </summary>
+    private RadzenDataGrid<TTea> TeaDataGrid { get; set; }
+
+    /// <summary>
     /// OnInitializedAsync Method
     /// </summary>
     /// <returns>Result of the Task</returns>
     protected override Task OnInitializedAsync()
     {
+        _showArchived = false;
+        
+        CollectionTeas = DatabaseContext.TTeas.Include(x => x.IdTypeNavigation)
+            .Include(x => x.IdVarietyNavigation)
+            .Include(x => x.IdRegionNavigation)
+            .Include(x => x.IdRegionNavigation.IdCountryNavigation)
+            .Include(x => x.IdProviderNavigation)
+            .Where(x => !x.TeaIsArchived.Value);
+        
         return base.OnInitializedAsync();
+    }
+    
+    void OnShowArchivedChange(bool isChecked)
+    {
+        _showArchived = isChecked;
+        if (_showArchived)
+        {
+            CollectionTeas = DatabaseContext.TTeas.Include(x => x.IdTypeNavigation)
+                .Include(x => x.IdVarietyNavigation)
+                .Include(x => x.IdRegionNavigation)
+                .Include(x => x.IdRegionNavigation.IdCountryNavigation)
+                .Include(x => x.IdProviderNavigation);
+        }
+        else
+        {
+            CollectionTeas = DatabaseContext.TTeas.Include(x => x.IdTypeNavigation)
+                .Include(x => x.IdVarietyNavigation)
+                .Include(x => x.IdRegionNavigation)
+                .Include(x => x.IdRegionNavigation.IdCountryNavigation)
+                .Include(x => x.IdProviderNavigation)
+                .Where(x => !x.TeaIsArchived.Value);
+        }
     }
 }
--- GestThé/GestThé/Pages/TeaDetails.razor.cs
+++ GestThé/GestThé/Pages/TeaDetails.razor.cs
@@ -5,6 +5,10 @@
 * Description : TeaDetails Component logic
 */
 
+using GestThéLib.Models.Database;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
 namespace GestThé.Pages;
 
 /// <summary>
@@ -12,5 +16,36 @@ namespace GestThé.Pages;
 /// </summary>
 public partial class TeaDetails
 {
+    /// <summary>
+    /// Id of the tea to show
+    /// </summary>
+    [Parameter]
+    public int? TeaId { get; set; }
     
+    /// <summary>
+    /// The tea to show info
+    /// </summary>
+    private TTea Tea { get; set; }
+
+    /// <summary>
+    /// OnInitializedAsync method
+    /// </summary>
+    protected override async Task OnInitializedAsync()
+    {
+        if (TeaId != null)
+            Tea = await GetTeaFromId();
+        else
+        {
+            Tea = new TTea();
+        }
+    }
+    
+    private async Task<TTea> GetTeaFromId()
+    {
+        return await DatabaseContext.TTeas.Include(x => x.IdTypeNavigation)
+            .Include(x => x.IdVarietyNavigation)
+            .Include(x => x.IdRegionNavigation)
+            .Include(x => x.IdRegionNavigation.IdCountryNavigation)
+            .Include(x => x.IdProviderNavigation).Where(x => x.IdTea == TeaId).FirstOrDefaultAsync();
+    }
 }
