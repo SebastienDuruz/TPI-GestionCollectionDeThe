4022fa179aee32c57bca3aa566be65468c836f65
Sébastien Duruz <sebastienduruz@protonmail.com>
2023-06-02 11:48:46 +0200

    Add a settings file, store the window position and size

--- GestThé/GestThéLib/Data/Electron/ElectronHandler.cs
+++ GestThé/GestThéLib/Data/Electron/ElectronHandler.cs
@@ -21,16 +21,28 @@ public static class ElectronHandler
     private static BrowserWindow MainWindow { get; set; }
     
     /// <summary>
+    /// Reader for the settings file
+    /// </summary>
+    private static SettingsReader SettingsReader { get; set; }
+    
+    /// <summary>
     /// Build the Electron Window
     /// </summary>
     public static async Task BuildElectronWindow()
     {
+        SettingsReader = new SettingsReader();
+        
         // Setup the Electron Window
         MainWindow = await ElectronNET.API.Electron.WindowManager.CreateWindowAsync(
             new BrowserWindowOptions()
             {
                 Title = "GestThé",
-                AutoHideMenuBar = true
+                AutoHideMenuBar = true,
+                Width = SettingsReader.SettingsValues.WindowWidth,
+                Height = SettingsReader.SettingsValues.WindowHeight,
+                X = SettingsReader.SettingsValues.WindowLeft,
+                Y = SettingsReader.SettingsValues.WindowTop,
+                Show = false
             });
 
         // Clear the cache before showing the window
@@ -38,6 +50,14 @@ public static class ElectronHandler
 
         // Add events to the Electron Window
         MainWindow.OnReadyToShow += () => MainWindow.Show();
+
+        // Save the windowSettings before the MainWindow is closed
+        ElectronNET.API.Electron.IpcMain.On("saveWindowSettings", async (e) =>
+        {
+            await SaveMainWindowSettings();
+            MainWindow.Destroy();
+            ElectronNET.API.Electron.App.Exit();
+        });
     }
 
     /// <summary>
@@ -60,5 +80,24 @@ public static class ElectronHandler
 
         // Return the selected folder if user selected one
         return result.Any() ? result[0] : null;
+    }
+    
+    /// <summary>
+    /// Save the current Electron values to settings file
+    /// </summary>
+    private static async Task SaveMainWindowSettings()
+    {
+        // Get the current value from MainWindow
+        int[] mainWindowSize = await MainWindow.GetSizeAsync();
+        int[] mainWindowPosition = await MainWindow.GetPositionAsync();
+
+        // Set the new values
+        SettingsReader.SettingsValues.WindowWidth = mainWindowSize[0];
+        SettingsReader.SettingsValues.WindowHeight = mainWindowSize[1];
+        SettingsReader.SettingsValues.WindowLeft = mainWindowPosition[0];
+        SettingsReader.SettingsValues.WindowTop = mainWindowPosition[1];
+
+        // Write the new Settings
+        SettingsReader.WriteSettings();
     }
 }
---
+++ GestThé/GestThéLib/Models/Electron/Settings.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * ETML
+ * Author : Sébastien Duruz
+ * Date : 02.06.2023
+ * Description : User Settings for the mainWindow
+ */
+
+namespace GestThéLib.Models.Electron;
+
+/// <summary>
+/// Class UserSettings
+/// </summary>
+public class Settings
+{
+    /// <summary>
+    /// Width of the main Window
+    /// </summary>
+    public int WindowWidth { get; set; } = 900;
+
+    /// <summary>
+    /// Height of the main Window
+    /// </summary>
+    public int WindowHeight { get; set; } = 650;
+
+    /// <summary>
+    /// Default TOP position of the main Window
+    /// </summary>
+    public int WindowTop { get; set; } = 100;
+
+    /// <summary>
+    /// Default LEFT position of the main Window
+    /// </summary>
+    public int WindowLeft { get; set; } = 100;
+}
